1be0381589a3dc069832dd51fbdbd6ce5510e014
logan <maxcl730@gmail.com>
2019-11-21 15:27:45 +0800

    add look scripts

--- work.sh
+++ work.sh
@@ -3,5 +3,5 @@ for ip_range in `cat china_ip_range.txt|grep -v ^#|grep ^$begin_with|awk '{print
     ip_begin=`echo $ip_range|awk -F ',' '{print $1}'`
     ip_end=`echo $ip_range|awk -F ',' '{print $2}'`
 	echo "Querying $ip_begin -- $ip_end >>>>>>>>"
-	#python ipcalc.py $ip_begin $ip_end
+	python ipcalc.py $ip_begin $ip_end
 done
