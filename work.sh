4bae8b1f04d7dcd071e16b1e15c796893708d7b9
logan <maxcl730@gmail.com>
2019-11-21 15:23:58 +0800

    add look scripts

---
+++ work.sh
@@ -0,0 +1,6 @@
+begin_with=$1
+for ip_range in `cat china_ip_range.txt|grep -v ^#|grep ^$begin_with|awk '{print $1","$2}'`;do
+    ip_begin=`echo $ip_range|awk -F ',' '{print $1}'`
+    ip_end=`echo $ip_range|awk -F ',' '{print $2}'`
+	echo $ip_begin $ip_end
+done
